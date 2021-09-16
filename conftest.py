1bb3029f9f8b763afa7059cb37ddb33c97c9cf18
Pavel Penikov <pavel@penikov.ru>
2021-09-16 19:11:16 +0400

    Added the fixture that closes Audacity with 'Close' button

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from base_app import BaseApp
 from app.download import Fetch
+from app.interactions import Audacity
+
 from conf import Conf
 
 
@@ -10,10 +12,22 @@ def fetch():
     return dnld
 
 
-
 @pytest.fixture(scope='module')
 def app():
+    """This fixture opens a new instance of BaseApp class, which runs Audacity"""
     exe = BaseApp().launch_latest()
     return exe
+
+
+@pytest.fixture(scope='function')
+def close_audacity(app):
+    """This fixture is closing Audacity"""
+    yield
+    Audacity(app).close_button()
+
+
+
+
+
 
 
--- tests/0001_welcome_screen_test.py
+++ tests/0001_welcome_screen_test.py
@@ -1,9 +1,13 @@
 from app.interactions import Welcome_screen
+from app.interactions import Audacity
 
 
-def test_click_get_help(app):
+def test_click_get_help(app, close_audacity):
     """The test checks that the 'how to get help' checkbox is appears at the first startup"""
     Welcome_screen(app).click_checkbox().close()
+
+
+
 
 
 

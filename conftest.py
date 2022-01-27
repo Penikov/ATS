c1d4164c9d3f03356ddc24a7ac0340bc1e9a99f8
Pavel Penikov <pavel@penikov.ru>
2022-01-27 09:31:04 +0400

    Cosmetic changes in conftest.py. Using the last version of Pytest instead of 4.6.11

--- conftest.py
+++ conftest.py
@@ -2,7 +2,6 @@ import pytest
 from base_app import BaseApp
 from app.download import Fetch
 from app.interactions import Audacity
-
 from conf import Conf
 
 
@@ -24,6 +23,8 @@ def close_audacity(app):
     """This fixture is closing Audacity"""
     yield
     Audacity(app).close_button()
+
+
 
 
 
